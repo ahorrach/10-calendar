200efe8f0daa3488404991aa2209e81755ccc616
ahorrach <ahorrach@gmail.com>
2023-11-20 22:03:18 +0100

    seccion 22

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,6 +1,38 @@
+import { Calendar } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+import { NavBar } from "../"
+import { addHours } from 'date-fns'
+
+import { getMessagesES, localizer } from '../../helpers'
+
+const events = [{
+  title: 'Cumpleanos',
+  notes: 'pastel',
+  start: new Date,
+  end: addHours(new Date(),2)
+}]
 
 export const CalendarPage = () => {
+  const eventStyleGetter = (event, start, end,isSelected) => {
+    console.log({ event,start,end,isSelected});
+  }
+
   return (
-    <div>CalendarPage</div>
+    <> 
+    <NavBar />
+    <div>
+    <Calendar
+      culture='es'
+      localizer={localizer}
+      events={events}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 'calc(100vh - 80px)' }}
+      messages={ getMessagesES()}
+      eventPropGetter={eventStyleGetter}
+    />
+  </div>
+    </>
   )
 }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,7 +5,7 @@ import { CalendarPage } from "../calendar";
 
 export const AppRouter = () => {
 
-    const authStatus = 'not-autenticated';
+    const authStatus = 'autenticated';
   
   return (
     <Routes>
