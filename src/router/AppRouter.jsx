eabf910b006be9de4eab4c0066555c8a3d23d1e5
ahorrach <ahorrach@gmail.com>
2023-11-20 20:36:58 +0100

    commit inicial MERN

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,24 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { LoginPage } from "../auth";
+import { CalendarPage } from "../calendar";
+
+
+export const AppRouter = () => {
+
+    const authStatus = 'not-autenticated';
+  
+  return (
+    <Routes>
+        {
+            (authStatus === 'not-autenticated')
+            ? <Route path="/auth/*" element= {<LoginPage /> } />
+            :  <Route path="/*" element= {<CalendarPage /> } />
+        }
+              
+
+        <Route path="/*" element= {<Navigate to="/auth/login" /> } />
+    
+    </Routes>
+
+  )
+}
